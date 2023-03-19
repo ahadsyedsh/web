ac769c2a3d392aa1702d278799a367667d47dcce
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-19 13:39:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
 import React from "react";
-import "../styles/App.css";
-// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
-    NavLink,
-    Link,
-} from "react-router-dom";
-import Info from "./Info";
-import Blog from "./Blog";
-import Home from "./Home";
-const App = () => {
+import { Link } from "react-router-dom";
+const Blog = () => {
     return (
-        <div id="main">
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <Home />
-                    </Route>
-                    <route path="/blog" exact>
-                        <Blog />
-                    </route>
-                    <route path="/info" exact>
-                        <Info />
-                    </route>
-                </Switch>
-            </Router>
+        <div id="blog-page">
+            <h1>Blog page</h1>
+            <h2>Checkout my awesome blogs </h2>
+            <h3>
+                <a href="https://www.newtonschool.co/post/top-13-web-developer-interview-questions">
+                    Web Development Interview questions
+                </a>
+            </h3>
+            <h3>
+                <a href="https://www.newtonschool.co/post/graph-data-structure-explained-with-examples">
+                    Graph Data Structures
+                </a>
+            </h3>
+            <Link to="/info">
+                <button id="blog-to-info"> Go to Info</button>
+            </Link>
+            <Link to="/">
+                <button id="blog-to-home"> Go to Home</button>
+            </Link>
         </div>
     );
 };
+export default Blog;
